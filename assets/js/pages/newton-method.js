21c9bcd2ca2714d9be549800c791d0a848e4bb5a
HyoyangIsBestHighschool <tony080417@gmail.com>
2026-07-11 00:23:17 +0900

    Add files via upload

---
+++ assets/js/pages/newton-method.js
@@ -0,0 +1,8 @@
+// 뉴턴법과 실근의 근사 시뮬레이션 로직
+// #sim-stage 안에 시각화를, #sim-controls 안의 컨트롤에 이벤트를 연결하세요.
+// 이 파일이 이 시뮬레이션의 담당 영역입니다.
+
+document.addEventListener('DOMContentLoaded', () => {
+  // TODO: 뉴턴법(접선을 이용한 실근의 근사) 구현
+  // 참고: x_(n+1) = x_n - f(x_n) / f'(x_n)
+});
